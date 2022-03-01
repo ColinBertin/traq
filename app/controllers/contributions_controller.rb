fa871a02c5bd85f5159612a74864b34046c4254c
Colin Bertin <colinbertin12@gmail.com>
2022-03-01 21:35:32 +0900

    contributions controller

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -4,8 +4,29 @@ class ContributionsController < ApplicationController
   end
 
   def new
+    @contribution = Contribution.new
+    authorize @contribution
+  end
+
+  def create
+    @location = Location.find(params[:id])
+    @contribution = Contribution.new(contribution_params)
+    @contribution.location = @location
+    @contributor.user = current_user
+    authorize @contribution
+    if @contribution.save
+      redirect_to users_contributions
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def contribution_params
+    params.require(:contribution).permit(:supply_type, :description, :quantity)
   end
 end
