c3469107128587161ead997130f54d51b2acc9c9
Colin Bertin <colinbertin12@gmail.com>
2022-03-03 21:49:49 +0900

    connecting rout edit & users contributions

---
+++ app/policies/user/contribution_policy.rb
@@ -0,0 +1,8 @@
+class User::ContributionPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.where(user: user)
+    end
+  end
+end
