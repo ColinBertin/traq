ed7d5ebf577a6f9a6437802965b59909eaa0fcac
Colin Bertin <colinbertin12@gmail.com>
2022-03-03 17:13:58 +0900

    end issues

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -33,5 +33,4 @@ class ContributionsController < ApplicationController
 
   def contribution_params
     params.require(:contribution).permit(:supply_type, :description, :quantity, :tag_list)
-    end
   end
